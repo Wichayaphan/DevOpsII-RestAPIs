a77662246a13f02e85ad5d5b2c4a25301c139d72
Wichayaphan <121413755+Wichayaphan@users.noreply.github.com>
2023-03-31 10:42:55 +0700

    Add files via upload

---
+++ python-put.py
@@ -0,0 +1,17 @@
+import requests
+api_url = "https://jsonplaceholder.typicode.com/todos/1"
+
+response = requests.get(api_url)
+print(response.json())
+
+todo = {
+    "userID" : 1,
+    "id" : 1,
+    "title" : "Wichayaphan Traithipthomrongchoke",
+    "completed" : True
+    }
+
+response = requests.put(api_url, json=todo)
+
+print(response.json())
+print(response.status_code)
